acf7510dd6c2e113ad2a47c3aa736427358677ce
Becky <becky@amc-pedss-mbp-3.ucdenver.pvt>
2022-08-15 12:06:34 -0600

    Add cellranger pipeline commands.  Note removed hard paths to sequence files and genome.

---
+++ workspace/cellranger_pipline/HB16hpf/cellranger_arc_pipeline.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#BSUB -J cellranger
+#BSUB -o logs/cellranger.%J.out
+#BSUB -e logs/cellranger.%J.err
+#BSUB -n 40
+#BSUB -R "select[mem>47] rusage[mem=47]"
+#BSUB -R "span[hosts=1]" 
+
+mkdir -p logs
+
+. /usr/share/Modules/init/bash
+module load modules modules-init
+module load cellranger_arc/1.0.1 
+
+cellranger-arc --version
+REF=cellranger_arc_genomes/zebrafishPlusGFP/GRCz11_with_chr
+
+SAMPLES=(HB16hpf)
+SAMPLE=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+ID=${SAMPLE}
+
+cellranger-arc count --id=${ID} --reference=${REF} --libraries=library.csv --localmem 47 --localcores 40
+
